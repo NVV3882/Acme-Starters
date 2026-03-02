f6fa46e85d5808bcc5ecf42e416da3fd81b1a3f3
Dsimong <dsimongomez@gmail.com>
2026-03-02 09:19:48 +0100

    Strategy added

---
+++ Acme-Starters-C/src/main/java/acme/entities/strategy/Strategy.java
@@ -0,0 +1,91 @@
+
+package acme.entities.strategy;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.constraints.ValidTicker;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Strategy extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidTicker
+	@Column(unique = true)
+	private String				ticker;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = ValidMoment.Constraint.ENFORCE_FUTURE)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String				moreInfo;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				draftMode;
+
+	// Derived attributes -----------------------------------------------------
+
+	@Transient
+	@Autowired
+	private StrategyRepository	repository;
+
+
+	@Transient
+	public Double monthsActive() {
+		return 0.0;
+
+	}
+
+	@Transient
+	public Double getExpectedPercentaje() {
+		Integer result = this.repository.getExpectedPercentaje();
+		return result;
+	}
+
+}
