29f14daa072c850b03c8973673a7e2b85220dfb6
Dsimong <dsimongomez@gmail.com>
2026-03-16 02:03:16 +0100

    Update failed

---
+++ Acme-Starters-C/src/main/java/acme/features/fundraiser/strategy/FundraiserStrategyPublishService.java
@@ -0,0 +1,54 @@
+
+package acme.features.fundraiser.strategy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.services.AbstractService;
+import acme.entities.strategy.Fundraiser;
+import acme.entities.strategy.Strategy;
+
+public class FundraiserStrategyPublishService extends AbstractService<Fundraiser, Strategy> {
+
+	@Autowired
+	private FundraiserStrategyRepository	repository;
+
+	private Strategy						strategy;
+
+
+	@Override
+	public void load() {
+		int id;
+		id = super.getRequest().getData("id", int.class);
+		this.strategy = this.repository.findStrategyById(id);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = this.strategy != null && this.strategy.getDraftMode() && this.strategy.getFundraiser().isPrincipal();
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.strategy, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+	}
+
+	@Override
+	public void validate() {
+		// TODO
+	}
+
+	@Override
+	public void execute() {
+		this.strategy.setDraftMode(false);
+		this.repository.save(this.strategy);
+	}
+
+	@Override
+	public void unbind() {
+		//TODO
+	}
+}
